82b7c4816e35aa1fb1783e056a6be3cadbfb147b
vikki1999 <982094509@qq.com>
2019-08-25 23:30:41 +0800

    checkGrids改为固定data

--- src/components/CheckGrids.js
+++ src/components/CheckGrids.js
@@ -12,11 +12,11 @@ class CheckGrids extends React.Component {
       pageSize: 10,
       allChecked: false,
       rowClicked: false,
-      data: []
+      data: this.getData()
     }
   }
   
-  // //express等后端接口请求数据
+  // //方案一：express等后端接口请求数据
   getData() {
   //   return fetch('http://localhost:3001/users', {
   //     headers: {
@@ -27,23 +27,26 @@ class CheckGrids extends React.Component {
   //   }).then(response => { 
   //     response.json().then(data => {this.setState({data:data,total:data.length}) }).catch(error => console.log(error))
   //   });
-  const data = queryRow('select * from sys_users');
-  console.log('data===>', data);
+
+  //方案二：直接调用MySQL连接
+  // const data = queryRow('select * from sys_users');
+  // console.log('data===>', data);
   
      
-    // return [
-    //   { "code": "FI-SW-01", "name": "Koi", "unitcost": 10.00, "status": "P", "listprice": 36.50, "attr": "Large", "itemid": "EST-1" },
-    //   { "code": "K9-DL-01", "name": "Dalmation", "unitcost": 12.00, "status": "P", "listprice": 18.50, "attr": "Spotted Adult Female", "itemid": "EST-10" },
-    //   { "code": "RP-SN-01", "name": "Rattlesnake", "unitcost": 12.00, "status": "P", "listprice": 38.50, "attr": "Venomless", "itemid": "EST-11" },
-    //   { "code": "RP-SN-01", "name": "Rattlesnake", "unitcost": 12.00, "status": "P", "listprice": 26.50, "attr": "Rattleless", "itemid": "EST-12" },
-    //   { "code": "RP-LI-02", "name": "Iguana", "unitcost": 12.00, "status": "P", "listprice": 35.50, "attr": "Green Adult", "itemid": "EST-13" },
-    //   { "code": "FL-DSH-01", "name": "Manx", "unitcost": 12.00, "status": "P", "listprice": 158.50, "attr": "Tailless", "itemid": "EST-14" },
-    //   { "code": "FL-DSH-01", "name": "Manx", "unitcost": 12.00, "status": "P", "listprice": 83.50, "attr": "With tail", "itemid": "EST-15" },
-    //   { "code": "FL-DLH-02", "name": "Persian", "unitcost": 12.00, "status": "P", "listprice": 23.50, "attr": "Adult Female", "itemid": "EST-16" },
-    //   { "code": "FL-DLH-02", "name": "Persian", "unitcost": 12.00, "status": "P", "listprice": 89.50, "attr": "Adult Male", "itemid": "EST-17" },
-    //   { "code": "AV-CB-01", "name": "Amazon Parrot", "unitcost": 92.00, "status": "P", "listprice": 63.50, "attr": "Adult Male", "itemid": "EST-18" }
-    // 
+    return [
+      { "code": "FI-SW-01", "name": "Koi", "unitcost": 10.00, "status": "P", "listprice": 36.50, "attr": "Large", "itemid": "EST-1" },
+      { "code": "K9-DL-01", "name": "Dalmation", "unitcost": 12.00, "status": "P", "listprice": 18.50, "attr": "Spotted Adult Female", "itemid": "EST-10" },
+      { "code": "RP-SN-01", "name": "Rattlesnake", "unitcost": 12.00, "status": "P", "listprice": 38.50, "attr": "Venomless", "itemid": "EST-11" },
+      { "code": "RP-SN-01", "name": "Rattlesnake", "unitcost": 12.00, "status": "P", "listprice": 26.50, "attr": "Rattleless", "itemid": "EST-12" },
+      { "code": "RP-LI-02", "name": "Iguana", "unitcost": 12.00, "status": "P", "listprice": 35.50, "attr": "Green Adult", "itemid": "EST-13" },
+      { "code": "FL-DSH-01", "name": "Manx", "unitcost": 12.00, "status": "P", "listprice": 158.50, "attr": "Tailless", "itemid": "EST-14" },
+      { "code": "FL-DSH-01", "name": "Manx", "unitcost": 12.00, "status": "P", "listprice": 83.50, "attr": "With tail", "itemid": "EST-15" },
+      { "code": "FL-DLH-02", "name": "Persian", "unitcost": 12.00, "status": "P", "listprice": 23.50, "attr": "Adult Female", "itemid": "EST-16" },
+      { "code": "FL-DLH-02", "name": "Persian", "unitcost": 12.00, "status": "P", "listprice": 89.50, "attr": "Adult Male", "itemid": "EST-17" },
+      { "code": "AV-CB-01", "name": "Amazon Parrot", "unitcost": 92.00, "status": "P", "listprice": 63.50, "attr": "Adult Male", "itemid": "EST-18" }
+    ]
   }
+
   handleRowCheck(row, checked) {
     let data = this.state.data.slice();
     let index = this.state.data.indexOf(row);
@@ -89,17 +92,17 @@ class CheckGrids extends React.Component {
               <CheckBox checked={this.state.allChecked} onChange={(checked) => this.handleAllCheck(checked)}></CheckBox>
             )}
           />
-          <GridColumn field="UserID" title="UserID" align="center"></GridColumn>
-          <GridColumn field="UserName" title="UserName" align="center"></GridColumn>
-          <GridColumn field="LoginPwd" title="LoginPwd" align="center"></GridColumn>
-          <GridColumn field="MobilePhone" title="MobilePhone" align="center"></GridColumn>
-          <GridColumn field="Email" title="Email" align="center" ></GridColumn>
-          <GridColumn field="Permissions" title="Permissions" align="center"></GridColumn>
-          <GridColumn field="Gender" title="Gender" align="center"></GridColumn>
-          <GridColumn field="Local" title="Local" align="center"></GridColumn>
+          <GridColumn field="code" title="code" align="center"></GridColumn>
+          <GridColumn field="name" title="name" align="center"></GridColumn>
+          <GridColumn field="unitcost" title="unitcost" align="center"></GridColumn>
+          <GridColumn field="status" title="status" align="center"></GridColumn>
+          <GridColumn field="listprice" title="listprice" align="center" ></GridColumn>
+          <GridColumn field="attr" title="attr" align="center"></GridColumn>
+          <GridColumn field="itemid" title="itemid" align="center"></GridColumn>
+          {/* <GridColumn field="Local" title="Local" align="center"></GridColumn>
           <GridColumn field="CreateTime" title="CreateTime" align="center"></GridColumn>
           <GridColumn field="Companyid" title="Companyid" align="center"></GridColumn>
-          <GridColumn field="Roleid" title="Roleid" align="center"></GridColumn>
+          <GridColumn field="Roleid" title="Roleid" align="center"></GridColumn> */}
         </DataGrid>
         <Pagination
               total={total}
